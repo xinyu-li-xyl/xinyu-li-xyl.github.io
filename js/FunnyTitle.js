787919bc57695945067dc4137e1ea5a93a5dcc3d
Xinyu Li <li.xin-yu@foxmail.com>
2024-04-21 00:54:34 +0800

    update FunnyTitle.js

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -2,16 +2,19 @@
  var OriginTitle = document.title;
  var titleTime;
  document.addEventListener('visibilitychange', function () {
-     if (document.hidden) {
-         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/Yafine/cdn@2.5/source/favicon.png");
-         document.title = '(●-`Д´-)快回来,粗大事啦!';
-         clearTimeout(titleTime);
-     }
-     else {
-         $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/Yafine/cdn@2.5/source/favicon.png");
-         document.title = '(Ő∀Ő)没事了嘿嘿...' + OriginTitle;
-         titleTime = setTimeout(function () {
-             document.title = OriginTitle;
-         }, 2000);
-     }
+    //  if (document.hidden) {
+    //      $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/Yafine/cdn@2.5/source/favicon.png");
+    //      document.title = '(●-`Д´-)快回来,粗大事啦!';
+    //      clearTimeout(titleTime);
+    //  }
+    //  else {
+    //      $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/Yafine/cdn@2.5/source/favicon.png");
+    //      document.title = '(Ő∀Ő)没事了嘿嘿...' + OriginTitle;
+    //      titleTime = setTimeout(function () {
+    //          document.title = OriginTitle;
+    //      }, 2000);
+    //  }
+    $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/Yafine/cdn@2.5/source/favicon.png");
+    document.title = '中华传统节日美食';
+    clearTimeout(titleTime);
  });
